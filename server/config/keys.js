958bdeaa41019316e2d8ddc7b4ba453f184977a4
thebombvass <mevomvas@bu.edu>
2019-11-25 11:08:54 +0100

    fixed mongodb issues

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://mvomvas:ZaqEdcBgt3@mvcluster-ovalr.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
 
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(cors());
 
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("Server is running on " + port + "port");
-});
+const db = require('./config/keys.js').mongoURI;
+console.log(db)
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+  
+mongoose.Promise = global.Promise;
+
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => console.log(`Server started on port ${port}`));
